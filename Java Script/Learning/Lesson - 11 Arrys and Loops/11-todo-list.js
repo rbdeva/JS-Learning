4ba32d9cd6fd614f17947382db6fdba61dc36269
Ravindra Babu Devabhaktuni <rbabu.devabhaktuni79@Gmail.com>
2024-11-29 23:50:25 -0600

    JS

--- Java Script/Learning/Lesson - 11 Arrys and Loops/11-todo-list.js
+++ Java Script/Learning/Lesson - 11 Arrys and Loops/11-todo-list.js
@@ -6,7 +6,13 @@ function renderTodoList(){
     for(let i=0;i<todoList.length;i++){
 
     let todo = todoList[i];
-    let html = `<p>${todo}</p>`;
+    let html = `
+    <p>
+    ${todo} 
+    <button onclick=" 
+    todoList.splice(${i}, 10);
+    "> Delete </button>
+     </p>`;
     todoListHTML += html;
     }
     console.log(todoListHTML);
